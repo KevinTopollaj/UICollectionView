5a21a88da137807f7dc4278eb246e166488d4f2f
Kevin Topollaj <kevintopollaj@gmail.com>
2021-04-10 23:52:01 +0200

    new Xcode project to test UICollectionViewCompositionLayout

---
+++ CollectionView/CollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionView
+//
+//  Created by Kevin Topollaj on 10.4.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
